d73fe47ee3777ddcdcf3c5232d4e5fcdcbf7525f
Konstantin <breaking700@gmail.com>
2021-03-06 17:25:40 +0300

    modified train for 4 layers CNN

--- train(4layers).py
+++ train(4layers).py
@@ -59,8 +59,11 @@ def create_dataset(filenames, batch_size):
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   x = tf.keras.layers.Conv2D(filters=8, kernel_size=3)(inputs)
+  x = tf.keras.layers.MaxPool2D()(x)
   x = tf.keras.layers.Conv2D(filters=8, kernel_size=3)(inputs)
+  x = tf.keras.layers.MaxPool2D()(x)
   x = tf.keras.layers.Conv2D(filters=8, kernel_size=3)(inputs)
+  x = tf.keras.layers.MaxPool2D()(x)
   x = tf.keras.layers.Conv2D(filters=8, kernel_size=3)(inputs)
   x = tf.keras.layers.MaxPool2D()(x)
   x = tf.keras.layers.Flatten()(x)
